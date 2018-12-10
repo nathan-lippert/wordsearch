b8741445125000c7af3e38ae94404716f23a9513
Nathan Lippert <nathan.lippert@outlook.com>
2018-12-10 14:12:47 -0600

    Passing open file hardcode case

--- wordsearch/__Init__.py
+++ wordsearch/__Init__.py
@@ -1,1 +1,3 @@
 """ __init__.py for wordsearch """
+
+from wordsearch.wordsearch import WordSearch
---
+++ wordsearch/wordsearch.py
@@ -0,0 +1,8 @@
+""" Defines the WordSearch class """
+
+
+class WordSearch(object):
+    """ Performs a wordsearch on a given input file """
+
+    def __init__(self, input_file):
+        self.words = ["BONES", "KHAN", "KIRK", "SCOTTY", "SPOCK", "SULU", "UHURA"]
